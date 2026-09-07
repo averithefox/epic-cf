4eb57cd28b83448270600c5b65960635eb8912eb
Averi <git@averi.me>
2026-09-07 18:40:51 +0000

    basic discord webhook

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,39 @@
-import { DurableObject } from "cloudflare:workers";
-
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
-
-/** A Durable Object's behavior is defined in an exported Javascript class */
-export class MyDurableObject extends DurableObject<Env> {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-	 *
-	 * @param ctx - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.jsonc
-	 */
-	constructor(ctx: DurableObjectState, env: Env) {
-		super(ctx, env);
-	}
-
-	/**
-	 * The Durable Object exposes an RPC method sayHello which will be invoked when a Durable
-	 *  Object instance receives a request from a Worker via the same method invocation on the stub
-	 *
-	 * @param name - The name provided to a Durable Object instance from a Worker
-	 * @returns The greeting to be sent back to the Worker
-	 */
-	async sayHello(name: string): Promise<string> {
-		return `Hello, ${name}!`;
-	}
-}
+import { verifyKey } from 'discord-interactions';
+import { type APIInteraction, APIInteractionResponse, InteractionResponseType, InteractionType, MessageFlags } from 'discord-api-types/v10';
 
 export default {
-	/**
-	 * This is the standard fetch handler for a Cloudflare Worker
-	 *
-	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.jsonc
-	 * @param ctx - The execution context of the Worker
-	 * @returns The response to be sent back to the client
-	 */
-	async fetch(request, env, ctx): Promise<Response> {
-		// Create a stub to open a communication channel with the Durable Object
-		// instance named "foo".
-		//
-		// Requests from all Workers to the Durable Object instance named "foo"
-		// will go to a single remote Durable Object instance.
-		const stub = env.MY_DURABLE_OBJECT.getByName("foo");
+	async fetch(req, env, ctx) {
+		const res = (status: number) => new Response('', { status });
+		const json = (body: APIInteractionResponse) =>
+			new Response(JSON.stringify(body), {
+				headers: { 'content-type': 'application/json;charset=UTF-8' },
+			});
 
-		// Call the `sayHello()` RPC method on the stub to invoke the method on
-		// the remote Durable Object instance.
-		const greeting = await stub.sayHello("world");
+		if (req.method !== 'POST') return res(404);
+		const sig = req.headers.get('x-signature-ed25519');
+		const timestamp = req.headers.get('x-signature-timestamp');
+		const body = await req.text();
+		if (!sig || !timestamp || !body) return res(404);
+		if (!(await verifyKey(body, sig, timestamp, env.DISCORD_PUBLIC_KEY))) return res(404);
+		const interaction: APIInteraction = JSON.parse(body);
 
-		return new Response(greeting);
+		switch (interaction.type) {
+			case InteractionType.Ping: {
+				return json({
+					type: InteractionResponseType.Pong,
+				});
+			}
+			case InteractionType.ApplicationCommand: {
+				return json({
+					type: InteractionResponseType.ChannelMessageWithSource,
+					data: {
+						content: 'rawr x3',
+						flags: MessageFlags.Ephemeral,
+					},
+				});
+			}
+		}
+
+		return res(404);
 	},
 } satisfies ExportedHandler<Env>;
